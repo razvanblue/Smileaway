c4bb6ba623ca992ad5fad684f91a17a65893ed0b
razvanblue <razvan.razvanblue@gmail.com>
2026-06-18 14:46:38 +0300

    Add character stats menu

---
+++ Source/Smileaway/UI/CharacterStatsWidget.cpp
@@ -0,0 +1,85 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CharacterStatsWidget.h"
+
+#include "Components/GridPanel.h"
+#include "Components/GridSlot.h"
+#include "Components/TextBlock.h"
+#include "Smileaway/Components/CharacterStats.h"
+
+void UCharacterStatsWidget::PopulateStatsGrid(UCharacterStats* InCharacterStats)
+{
+	if (!InCharacterStats || !StatsGrid)
+	{
+		return;
+	}
+	
+	CharacterStats = InCharacterStats;
+
+	int32 Row = 2;
+
+	const UTextBlock* Template = Cast<UTextBlock>(StatsGrid->GetChildAt(1));
+	
+	for (int32 StatIndex = 0; StatIndex < static_cast<int32>(EStats::MAX); ++StatIndex)
+	{
+		const EStats Stat = static_cast<EStats>(StatIndex);
+
+		const float FinalValue = CharacterStats->GetStat(Stat);
+		const float BaseValue = CharacterStats->GetBaseStat(Stat);
+
+		UTextBlock* NameText = CreateStyledText(Template, GetStatText(static_cast<uint8>(Stat)));
+		UTextBlock* FinalText = CreateStyledText(Template, FText::AsNumber(FinalValue));
+		UTextBlock* BaseText = CreateStyledText(Template, FText::Format(FText::FromString("({0})"), BaseValue));
+
+		StatsGrid->AddChildToGrid(NameText, Row, 0);
+		StatsGrid->AddChildToGrid(FinalText, Row, 1);
+		StatsGrid->AddChildToGrid(BaseText, Row, 2);
+		
+		BaseStats[StatIndex] = BaseText;
+		FinalStats[StatIndex] = FinalText;
+
+		++Row;
+	}
+}
+
+void UCharacterStatsWidget::UpdateStats()
+{
+	if (!CharacterStats || !IsValid(FinalStats[0]))
+	{
+		return;
+	}
+	
+	int32 Row = 2;
+	
+	for (int32 StatIndex = 0; StatIndex < static_cast<int32>(EStats::MAX); ++StatIndex)
+	{
+		const EStats Stat = static_cast<EStats>(StatIndex);
+
+		const float FinalValue = CharacterStats->GetStat(Stat);
+		FinalStats[StatIndex]->SetText(FText::AsNumber(FinalValue));
+		
+		const float BaseValue = CharacterStats->GetBaseStat(Stat);
+		BaseStats[StatIndex]->SetText(FText::Format(FText::FromString("({0})"), BaseValue));
+
+		++Row;
+	}
+}
+
+UTextBlock* UCharacterStatsWidget::CreateStyledText(
+	const UTextBlock* Template,
+	const FText& Text)
+{
+	UTextBlock* NewText = DuplicateObject<UTextBlock>(Template, this);
+
+	NewText->SetText(Text);
+
+	return NewText;
+}
+
+const FText UCharacterStatsWidget::GetStatText(uint8 Stat)
+{
+	static const FText StatNames[] = {FText::FromString("Max HP"), FText::FromString("Attack"), FText::FromString("Speed"), FText::FromString("Knockback")};
+	
+	return StatNames[Stat];
+}
---
+++ Source/Smileaway/UI/CharacterStatsWidget.h
@@ -0,0 +1,46 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Smileaway/DataAssets/CharacterStat.h"
+#include "CharacterStatsWidget.generated.h"
+
+class UCharacterStats;
+class UGridPanel;
+class UTextBlock;
+/**
+ * 
+ */
+UCLASS()
+class SMILEAWAY_API UCharacterStatsWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	
+	UFUNCTION(BlueprintCallable)
+	void PopulateStatsGrid(UCharacterStats* InCharacterStats);
+
+	UFUNCTION(BlueprintCallable)
+	void UpdateStats();
+	
+protected:
+	
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+	TObjectPtr<UGridPanel> StatsGrid;
+	
+private:
+	
+	UTextBlock* CreateStyledText(const UTextBlock* Template, const FText& Text);
+	
+	static const FText GetStatText(uint8 Stat);
+	
+	UPROPERTY(Transient)
+	TObjectPtr<UCharacterStats> CharacterStats;
+	
+	// Stored for updating text
+	TStatArray<UTextBlock*> BaseStats;
+	TStatArray<UTextBlock*> FinalStats;
+};
