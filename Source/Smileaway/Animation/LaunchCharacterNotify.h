c8753d81e31c1b7cd6d62ec85f7987c7d39247dd
razvanblue <razvan.razvanblue@gmail.com>
2026-06-19 21:41:05 +0300

    Use skill data for dodge and heavy attack; polish movement during special attacks

---
+++ Source/Smileaway/Animation/LaunchCharacterNotify.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "LaunchCharacterNotify.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SMILEAWAY_API ULaunchCharacterNotify : public UAnimNotify
+{
+	GENERATED_BODY()
+	
+	virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
+	
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Launch")
+	FVector LaunchVelocity = FVector(1000.f, 0.f, 300.f);
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Launch")
+	bool bIsControllerRelative = false;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Launch")
+	bool bXYOverride = true;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Launch")
+	bool bZOverride = true;
+
+};
---
+++ Source/Smileaway/Animation/LockRotationNotifyState.cpp
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LockRotationNotifyState.h"
+
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void ULockRotationNotifyState::NotifyBegin(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+                                           float TotalDuration, const FAnimNotifyEventReference& EventReference)
+{
+	ACharacter* Character = Cast<ACharacter>(MeshComp->GetOwner());
+	if (!Character) return;
+
+	
+	bUseControllerRotationYaw = Character->bUseControllerRotationYaw;
+	bOrientRotationToMovement = Character->GetCharacterMovement()->bOrientRotationToMovement;
+	
+	Character->bUseControllerRotationYaw = false;
+	Character->GetCharacterMovement()->bOrientRotationToMovement = false;
+	
+	Super::NotifyBegin(MeshComp, Animation, TotalDuration, EventReference);
+}
+
+void ULockRotationNotifyState::NotifyEnd(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+	const FAnimNotifyEventReference& EventReference)
+{
+	ACharacter* Character = Cast<ACharacter>(MeshComp->GetOwner());
+	if (!Character) return;
+
+	Character->bUseControllerRotationYaw = bUseControllerRotationYaw;
+	Character->GetCharacterMovement()->bOrientRotationToMovement = bOrientRotationToMovement;
+	
+	Super::NotifyEnd(MeshComp, Animation, EventReference);
+}
